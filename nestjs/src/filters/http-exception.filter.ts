3444297ee0676d903541316996be32af090a3b36
Thet Htet Aung <thethtetaung17@gmail.com>
2020-06-22 14:40:10 +0630

    add response and req from express

--- nestjs/src/filters/http-exception.filter.ts
+++ nestjs/src/filters/http-exception.filter.ts
@@ -5,8 +5,8 @@ import { Request, Response } from 'express';
 export class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
-        const response = ctx.getResponse();
-        const request = ctx.getRequest();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
         const status = exception.getStatus();
         if (response && response.status) {
             response
